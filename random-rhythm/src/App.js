636fe5f9486792bf3597dbc7f71287ff391985d6
mattchley <mattchley@gmail.com>
2021-09-21 13:20:23 -0700

    Random Button Component returns the Key and Scale values after being pressed. Now I need to connect these values to set the state of scale and key

--- random-rhythm/src/App.js
+++ random-rhythm/src/App.js
@@ -16,9 +16,10 @@ function App() {
 
   return (
     <div className="App">
-      <Key onChange={(value) => setKey(value)} value={{ keyArray: keyArray }}/>
+      <Key onChange={(value) => setKey(value)} value={{ keyArray: keyArray }} />
       <Scale onChange={(value) => setScale(value)} value={{ scaleArray: scaleArray }} />
-      <RandomButton value={{ scaleArray: scaleArray, keyArray: keyArray }} />
+      {/* trying to connect the props from the random button to the use state to allow for a genereated staff on click. Partially there because on click shows the props passing but not selected */}
+      <RandomButton value={{ scaleArray: scaleArray, keyArray: keyArray }} onClick={(value) => console.log(`hit ${value}`)}/>
       <Staff value={{ scale: [scale, setScale], key: [key, setKey], keyArray: keyArray }} />
 
 
--- random-rhythm/src/Components/randomButton.js
+++ random-rhythm/src/Components/randomButton.js
@@ -5,20 +5,21 @@ const RandomButton = props => {
     // this component allows for the user to randomly generate a Staff with key signature and scale.
 
     const handleSubmit = () => {
+
         // this variable sets the terrishold for the Key generated
         let x = Math.ceil(Math.random() * props.value.keyArray.length - 1)
         // this variable sets the terrishold for the Scale generated
         let y = Math.ceil(Math.random() * props.value.scaleArray.length - 1)
 
-        // this will return Key generated
-        console.log(props.value.keyArray[x])
-        // this will return Scale generated
-        console.log(props.value.scaleArray[y])
+        return [props.value.keyArray[x], props.value.scaleArray[y]]
+
+        // // this will return Key generated props.value.keyArray[x]
+        // // this will return Scale generated props.value.scaleArray[y]
     }
 
     return (
         <div>
-            <button onClick={handleSubmit}>Random Key/Scale</button>
+            <button onClick={e => props.onClick(handleSubmit())}>Random Key/Scale</button>
         </div>
     )
 
