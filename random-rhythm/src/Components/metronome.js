8c12f677f6b6d736d608dd7007de57b7a2044f2a
mattchley <mattchley@gmail.com>
2021-10-03 13:57:02 -0700

    added basic functionality for metronome to determine time signature. Still need to connect time signature to staff and random button components.

--- random-rhythm/src/Components/metronome.js
+++ random-rhythm/src/Components/metronome.js
@@ -4,11 +4,17 @@ import React, { useState } from "react";
 const Metronome = props => {
     const [count, setCount] = useState(1)
     let bpm = props.value.bpm
+    let timeSig = props.value.timeSig
     let milliseconds = 60000 / bpm
 
 
     const tester = () => {
         // needs a reset interval function
+        if (timeSig == '3/4') {
+            console.log('time sig is 3/4')
+        } else {
+            console.log('time sig is 4/4')
+        }
         let i = 1
         const testbeat = () => {
             i++
